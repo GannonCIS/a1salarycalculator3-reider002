e976c2b45489fb488ff7426d108c780ede0fc7b3
Ashley Reider <reider002@gannon.edu>
2020-02-14 00:21:20 -0500

    completed initial structure for salaryCalculator

---
+++ src/assignment1salarycalculator/salaryCalculator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+/**
+ *
+ * @author 18147
+ */
+public class salaryCalculator {
+    
+    public int salary;
+    public int hourlyRate;
+    double percent;
+    
+    public void calcHourlyRate(int salary){
+        
+    }
+    public void calcSalary(int hourlyRate){
+        
+    }
+    public void monthlyAfterTax(int salary, double percent){
+        
+    }
+}
