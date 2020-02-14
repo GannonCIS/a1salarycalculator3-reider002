61fcb0e1c09cb09e5df91e32cf1def406a36864e
Ashley Reider <reider002@gannon.edu>
2020-02-14 01:51:12 -0500

    completed basic logic structure, tested in main

--- src/assignment1salarycalculator/Assignment1SalaryCalculator.java
+++ src/assignment1salarycalculator/Assignment1SalaryCalculator.java
@@ -15,7 +15,9 @@ public class Assignment1SalaryCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        salaryCalculator bob = new salaryCalculator();
+        bob.test();
+
     }
-    
+
 }
--- src/assignment1salarycalculator/salaryCalculator.java
+++ src/assignment1salarycalculator/salaryCalculator.java
@@ -10,18 +10,37 @@ package assignment1salarycalculator;
  * @author 18147
  */
 public class salaryCalculator {
-    
-    public int salary;
-    public int hourlyRate;
-    double percent;
-    
-    public void calcHourlyRate(int salary){
-        
+
+    public int yearlySalary = 100000;
+    private int hours = 1200;
+    public double hourlyRate = 11.5;
+    double percent = 0.6;
+
+    public double calcHourlyWage() {
+
+        if (hours >= 40) {
+            hourlyRate = yearlySalary / 52 / hours;
+        } else {
+            int extraHours = hours - 40;
+            yearlySalary = (int) ((40 * hourlyRate) + (extraHours * hourlyRate));
+        }
+        return hourlyRate;
     }
-    public void calcSalary(int hourlyRate){
-        
+
+    public int calcSalary() {
+        yearlySalary = (int) (hours * hourlyRate * 52);
+        return yearlySalary;
     }
-    public void monthlyAfterTax(int salary, double percent){
-        
+
+    public int monthlyAfterTax() {
+        int monthlySalary = yearlySalary / 12;
+        int monthAfterTax = (int) (percent * monthlySalary);
+        return monthAfterTax;
+    }
+
+    public void test() {
+        calcHourlyWage();
+        calcSalary();
+        monthlyAfterTax();
     }
 }
